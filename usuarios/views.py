48bee257a4177180bca9b1d23a0e254cc162af18
Luana Duyen <lu.nguyen@outlook.com>
2021-10-11 18:52:00 -0300

    Add cadastro, login and base.html

--- usuarios/views.py
+++ usuarios/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def cadastro(resquest):
-    return HttpResponse('teste')
+    return render(resquest, 'cadastro.html') 
+
+def login(request):
+    return render(request, 'login.html')
