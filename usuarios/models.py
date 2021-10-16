604647c53fa3161a025a9126f37b89c67b7610d9
Luana Duyen <lu.nguyen@outlook.com>
2021-10-16 17:15:37 -0300

    Add useradmin

--- usuarios/models.py
+++ usuarios/models.py
@@ -4,3 +4,6 @@ class Usuario(models.Model):
     nome = models.CharField(max_length=152)
     email = models.EmailField()
     senha = models.CharField(max_length=64) 
+
+    def __str__(self) -> str:
+        return self.nome
